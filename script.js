d6d2be2e8a9783a6744ad5a9382989eccd1531e9
abdul <abduldesigns24@gmail.com>
2025-02-15 07:23:36 +0530

    Refactor ToDo app functionality to use arrays and local storage for task management

--- script.js
+++ script.js
@@ -24,35 +24,80 @@ let parent = document.querySelector(".parent");
 //   });
 // });
 
-button.addEventListener("click", () => {
-  if (input.value === "") {
-    alert("enter the value");
-  } else {
+//------working code method 1-----------
+
+// button.addEventListener("click", () => {
+//   if (input.value === "") {
+//     alert("enter the value");
+//   } else {
+//     let li = document.createElement("li");
+//     let span = document.createElement("span");
+//     li.innerHTML = input.value;
+//     parent.appendChild(li);
+//     span.innerHTML = "\u00d7";
+//     li.appendChild(span);
+//     setData();
+//   }
+//   input.value = "";
+// });
+
+// //event listener using eventlistner
+// parent.addEventListener("click", (e) => {
+//   if (e.target.tagName === "SPAN") {
+//     e.target.parentElement.remove();
+//     setData();
+//   }
+// });
+
+// //using local storage
+// function setData() {
+//   localStorage.setItem("data", parent.innerHTML);
+// }
+
+// function showData() {
+//   parent.innerHTML = localStorage.getItem("data");
+// }
+// showData();
+
+//------- Method 2 --------
+//------- using arrays and Local Storage ------
+//--bit complex--
+
+//creating an array to store list items as objects
+let arr = [];
+
+//pusing list data as object inside array
+button.addEventListener("click", (e) => {
+  let inputvalue = input.value.trim();
+  arr.push({ task: inputvalue, completed: false });
+  //calling update function
+  updateTask();
+  e.preventDefault();
+});
+
+const toglFun = (indx) => {
+  arr[indx].completed = !arr[indx].comple;
+};
+
+function updateTask() {
+  //clearing existing items
+  parent.innerHTML = " ";
+
+  //looping over array of objects to add list items
+  arr.forEach((val, indx) => {
+    //create an li element to inject the html code with task
     let li = document.createElement("li");
-    let span = document.createElement("span");
-    li.innerHTML = input.value;
-    parent.appendChild(li);
-    span.innerHTML = "\u00d7";
-    li.appendChild(span);
-    setData();
-  }
-  input.value = "";
-});
 
-//event listener using eventlistner
-parent.addEventListener("click", (e) => {
-  if (e.target.tagName === "SPAN") {
-    e.target.parentElement.remove();
-    setData();
-  }
-});
-
-//using local storage
-function setData() {
-  localStorage.setItem("data", parent.innerHTML);
+    //inner html code with data insertion
+    li.innerHTML = `
+    <input type="checkbox" clas="checkbox" ${
+      val.completed ? "completed" : ""
+    } />
+    <p> ${val.task} </p> `;
+    li.addEventListener("change", () => {
+      toglFun(indx);
+      console.log("clicked");
+    });
+    parent.append(li);
+  });
 }
-
-function showData() {
-  parent.innerHTML = localStorage.getItem("data");
-}
-showData();
